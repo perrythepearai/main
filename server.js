1bf698aa5fb24eb8cd95bb8de2ae30eace1aee2d
perry <partners@perrythepear.com>
2025-02-26 17:09:27 +0300

    Change Server

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const pool = new pg.Pool({
 // Test database connection
 pool.connect((err, client, release) => {
     if (err) {
-        console.error('❌ Database connection:', err);
+        console.error('❌ Database connection issue:', err);
         return;
     }
     console.log('✅ Database connected successfully');
